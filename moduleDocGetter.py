7191870db269859c04eeec86980a0cb20b1914c8
sav112 <siddharth.vasu12@imperial.ac.uk>
2016-08-07 20:42:43 +0100

    changed try-catch to with block

--- moduleDocGetter.py
+++ moduleDocGetter.py
@@ -3,12 +3,8 @@ import sys
 import os
 
 def to_file(name,s):
-    try:
-        f = file(name + ".txt",'w')
-        f.write(s)
-        f.close()
-    except:
-        pass
+    with open(name + ".txt",'w') as f:
+        f.write(str(s))
 
 def main():
     if len(sys.argv) < 2:
@@ -23,8 +19,10 @@ def main():
             lib = lib.__dict__[parts[i]]
     os.makedirs(libname)
     d = dir(lib)
+    docs = 0
     for i in d:
         if('func_doc' in dir(lib.__dict__[i])):
+            docs += 1
             to_file(libname + "/" + i,lib.__dict__[i].func_doc)
-
+    print "{0} function {1} obtained from module {2}".format(docs,"document" if docs == 1 else "documents",libname)
 main()
